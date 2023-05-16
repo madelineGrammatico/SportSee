4b04631825a88d851684a373962b6587007abcec
Madeline <grammaticomadeline@hotmail.fr>
2023-05-16 16:54:35 +0200

    fix(app) : add stylised 500error page

--- src/layouts/500Page/500Page.tsx
+++ src/layouts/500Page/500Page.tsx
@@ -1,7 +1,12 @@
+import styles from './500Page.module.css'
+
 export function Page500() {
     return(
         <main>
-            <h2>500</h2>
+            <div className={styles["page500__message"]}>
+            <h2>Une erreur interne du serveur est survenue</h2>
+            <a href="/" className={styles["page500__link"]}>Actualiser</a>
+            </div>
         </main>
     )
 }
